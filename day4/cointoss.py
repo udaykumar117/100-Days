9a04bf91a88a36dd08324ec0cfe6ff4b6e56a051
Uday Kumar <72568252+udaykumar117@users.noreply.github.com>
2021-11-13 17:16:15 -0500

    Delete day4 directory

--- day4/cointoss.py
+++
@@ -1,8 +0,0 @@
-import random
-
-random_coin = random.randint(0,1)
-
-if random_coin==1:
-    print("Heads")
-else:
-    print("Tails")
--- day4/list.py
+++
@@ -1,8 +0,0 @@
-states_of_America = ["AL","AK","AZ","AR" , "CA"]
-
-print(states_of_America)
-states_of_America[0]="NY"
-print(states_of_America)
-states_of_America.append("IL")
-states_of_America.extend(["MI", "MA", "OH"])
-print(states_of_America)

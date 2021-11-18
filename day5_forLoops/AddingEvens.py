036db82ae2e25dd09f6cf335a022fff55069bd22
Uday Kumar <72568252+udaykumar117@users.noreply.github.com>
2021-11-18 08:44:16 -0500

    Delete AddingEvens.py

--- day5_forLoops/AddingEvens.py
+++
@@ -1,8 +0,0 @@
-#Write your code below this row 👇
-evenSum = 0
-for number in range(1, 101):
-    if number%2 ==0:
-        print(number)
-        evenSum += number
-print(f"Sum of even numbers is: {evenSum}")
-
